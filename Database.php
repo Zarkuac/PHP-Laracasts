71ed09a2cf1511793c1c1f3b2167ad7476045b28
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-07-15 17:06:58 +0400

    authorization logics & refactor code

--- Database.php
+++ Database.php
@@ -3,6 +3,7 @@
 class Database {
 
     public $connection;
+    public $statement;
 
     public function __construct($config, $username='root', $password='')
     {
@@ -17,11 +18,30 @@ class Database {
     public function query($query, $params = [])
     {
 
-        $statement = $this->connection-> prepare($query);
+       $this->statement = $this->connection-> prepare($query);
 
-        $statement-> execute($params);
+       $this->statement->execute($params);
  
+        return $this;
+    }
 
-        return $statement;
+    public function get() {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+
+        return $this->statement->fetch();
+
+    }
+
+    public function findOrFail() {
+        $result = $this->find();
+    
+        if(!$result) {
+            abort();
+        }
+        return $result;
     }
 }
--- controllers/note.php
+++ controllers/note.php
@@ -8,16 +8,9 @@ $currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', [
     'id' => $_GET['id']
-    ])->fetch();
+    ])->findOrFail();
 
+    authorize($note['user_id'] == $currentUserId);
     
-    if (!$note) {
-        abort();
-    }
-
-    
-    if($note['user_id'] != $currentUserId) {
-        abort(Response::FORBIDDEN);
-    }
 
 require "views/note.view.php";
