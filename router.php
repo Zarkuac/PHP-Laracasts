11b648863534c9fe30ba5c0068cacb710aa76a37
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-07-15 17:45:35 +0400

    forms and requests

--- router.php
+++ router.php
@@ -1,16 +1,8 @@
 <?php 
 
+$routes = require('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/contact' => 'controllers/contact.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/about' => 'controllers/about.php'
-];
-
 
 function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
