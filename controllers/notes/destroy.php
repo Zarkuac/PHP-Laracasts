ae77a7cb522935ac5f4151120bd40d814b44f593
zarkuac <tsotne.zarkua.1@btu.edu.ge>
2024-07-20 21:46:57 +0400

    + first service container

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -1,12 +1,12 @@
 <?php
 
+use Core\App;
 use Core\Database;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+$db = App::resolve(Database::class);
 
 
-$currentUserId = 1;
+$currentUserId = 2;
 
 
    
