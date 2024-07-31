b6291e91d9fbd22c2d761ecf52519c46f2ccbf66
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-07-31 12:04:13 +0400

    database changed to mySQL

--- Core/Database.php
+++ Core/Database.php
@@ -9,7 +9,7 @@ class Database {
     public $connection;
     public $statement;
 
-    public function __construct($config, $username='root', $password='')
+    public function __construct($config, $username='root', $password='123qweQWE!@#')
     {
         $dsn = 'mysql:' . http_build_query($config,'',';');
 
--- config.php
+++ config.php
@@ -1,10 +1,20 @@
 <?php 
 
 
+// return [
+//     'database' => 
+//     [
+//     'host' => 'localhost',
+//     'port' => 3306,
+//     'dbname' => 'laracast_db',
+//     'charset' => 'utf8mb4'
+//     ]
+// ];
+
 return [
     'database' => 
     [
-    'host' => 'localhost',
+    'host' => '127.0.0.1',
     'port' => 3306,
     'dbname' => 'laracast_db',
     'charset' => 'utf8mb4'
